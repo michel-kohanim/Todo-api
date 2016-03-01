e72e0eac1696448d0d9b832dcfab1d3e7b6cbb2c
MKohanim <michel.kohanim@gmai.com>
2016-03-01 14:52:34 -0800

    variable things

---
+++ variable-playground.js
@@ -0,0 +1,17 @@
+var person = {
+	name: 'Michel',
+	age: 50
+}
+
+function updatePerson(person)
+{
+	person.age = 49;
+	debugger;
+}
+
+updatePerson(person);
+console.log(person);
+
+//array exmple
+var grades =[80, 90];
+
