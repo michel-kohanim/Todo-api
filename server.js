245e113b6901fe23dc738e420954a7f09e0af60c
MKohanim <michel.kohanim@gmai.com>
2016-03-01 10:22:00 -0800

    Added todos to the API

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [
+{
+	id: 1,
+	description: 'Meet mom for lunch',
+	completed: false,
+},
+{
+	id: 2,
+	description: 'Go to market',
+	completed: false,
+},
+{
+	id: 3,
+	description: 'Learn node.js',
+	completed: true,
+},
+];
+
+app.get('/todos', function(req, res){
+	res.json(todos);
+});
+
+app.get('/todos/:id', function(req, res){
+	//console.log('param is ' + req.params.id + ' lenght of the array ' + todos.length);
+	for (var i=0; i<todos.length; i++)
+	{
+		//you can also use parseInt(string, radix);
+	//	console.log(todos[i]);
+		if (todos[i].id == req.params.id) // === Number(req.params.id) == req.params.id) //)
+		{
+			res.json(todos[i]);
+			return;
+		}
+	}
+	res.status(404).send('Id of ' + req.params.id + ' not found!');
+});
 
 app.get('/', function(req, res){
 	res.send('Hello world' );
