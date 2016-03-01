7bd2e560f5d4332ecdf9967367591c940b3eb50e
MKohanim <michel.kohanim@gmai.com>
2016-03-01 14:13:23 -0800

    add put todos

--- server.js
+++ server.js
@@ -58,6 +58,51 @@ app.delete('/todos/:id', function(req, res){
 		res.status(404).json({"error":"no todo found with that id"});
 });
 
+app.put('/todos/:id', function(req, res){
+	//console.log('param is ' + req.params.id + ' lenght of the array ' + todos.length);
+	/*for (var i=0; i<todos.length; i++)
+	{
+		//you can also use parseInt(string, radix);
+	//	console.log(todos[i]);
+		if (todos[i].id == req.params.id) // === Number(req.params.id) == req.params.id) //)
+		{
+			res.json(todos[i]);
+			return;
+		}
+	}*/
+	var todoId = parseInt(req.params.id);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+	if (matchedTodo)
+	{
+		var body = _.pick(req.body, 'description', 'completed');
+		var validAttributes = {};
+		if(body.hasOwnProperty('completed') && _.isBoolean(body.completed))
+		{
+			validAttributes.completed=body.completed;
+		}else if (body.hasOwnProperty('completed'))
+		{
+			res.status(400).send();
+			return;
+
+		}
+
+		if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length >0)
+		{
+			validAttributes.description=body.description;
+
+		}else if (body.hasOwnProperty('description'))
+		{
+			res.status(400).send();
+			return;
+		}
+		//everything went well. update the attributes
+		_.extend(matchedTodo, validAttributes);
+		res.json(matchedTodo);
+	}
+	else
+		res.status(404).json({"error":"no todo found with that id"});
+});
+
 app.post('/todos', function(req, res){
 	//pick only description and completed
 	var body = _.pick(req.body, 'description', 'completed');
